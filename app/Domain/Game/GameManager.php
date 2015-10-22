2e337ee9d3088e94677046ba0bebed46edad2c1f
Mark Fee <git@markfee.com>
2015-10-22 06:26:23 +0100

    GameManager Stub and tests created - works with Mock of player to check that a game can be started

--- app/Domain/Game/GameManager.php
+++ app/Domain/Game/GameManager.php
@@ -2,5 +2,25 @@
 
 
 class GameManager {
+    private $players = [];
+
+    function addPlayer($player)
+    {
+        $this->players[] = $player;
+    }
+
+    function isReadyToStart()
+    {
+        if (count($this->players) != 2) {
+            return false;
+        }
+
+        foreach($this->players as $player) {
+            if (false === $player->isReadyToPlay()) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 }
--- tests/Functional/GameManagerTest.php
+++ tests/Functional/GameManagerTest.php
@@ -1,6 +1,7 @@
 <?php namespace Tests\Functional;
 
 use Garmential\Game\GameManager;
+use Mockery\Mock;
 
 class GameManagerTest  extends \TestCase {
 
@@ -9,6 +10,7 @@ class GameManagerTest  extends \TestCase {
         $gameManager = new GameManager;
         $this->assertTrue($gameManager instanceof GameManager);
     }
+
    /*
     * When I start a simple game I need two players,each of whom should be
     * given 5 randomly generated Warriors.
@@ -18,5 +20,37 @@ class GameManagerTest  extends \TestCase {
     * considered the winner.
    */
 
+    public function test_a_game_cannot_start_without_two_players()
+    {
+        $gameManager = new GameManager;
+        $this->assertFalse($gameManager->isReadyToStart(), "The game should not be ready to start until two players are added.");
+
+        $player1Mock = new Mock('Player');
+        $player1Mock->shouldReceive('isReadyToPlay')->once()->andReturn(true);
+
+        $player2Mock = new Mock('Player');
+        $player2Mock->shouldReceive('isReadyToPlay')->once()->andReturn(true);
+
+        $gameManager->addPlayer($player1Mock);
+        $gameManager->addPlayer($player2Mock);
+
+        $this->assertTrue($gameManager->isReadyToStart(), "The game should now be ready when two players are added and are both ready.");
+    }
+
+    public function test_a_game_cannot_start_without_two_players_who_are_both_ready()
+    {
+        $gameManager = new GameManager;
+
+        $player1Mock = new Mock('Player');
+        $player1Mock->shouldReceive('isReadyToPlay')->once()->andReturn(true);
+
+        $player2Mock = new Mock('Player');
+        $player2Mock->shouldReceive('isReadyToPlay')->once()->andReturn(false);
+
+        $gameManager->addPlayer($player1Mock);
+        $gameManager->addPlayer($player2Mock);
+
+        $this->assertFalse($gameManager->isReadyToStart(), "The game should not be ready when both players aren't ready.");
+    }
 
 }
